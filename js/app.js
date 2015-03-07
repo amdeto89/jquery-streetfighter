c47c03648d20b65291f28b43a98dfe78f42a2940
Amdeto89 <amietoole@gmail.com>
2015-03-06 20:42:43 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -17,14 +17,32 @@ $(document).ready(function(){
             function(){
                 $(this).hide();
                 $(this).css('left', '520px');
-            });
+            }
+           );
     })
     .mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
+        $('hadouken').hide();
     });
-
+	$(document).on("keydown",function(event){
+		if (event.which == 88){
+			$(".ryu-still").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").hide();
+			$(".ryu-cool").show();
+		};
+	});
+	$(document).on("keyup",function(event){
+		if (event.which == 88){
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").hide();
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		};
+	});
 });
+
 function playHadouken() {
         $('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
